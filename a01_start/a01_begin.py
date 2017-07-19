c20fd7d05c06710467ced13107622084418a1ed5
JohnBay <moneymashi@gmail.com>
2017-07-19 09:42:48 +0900

    ++ 0717 python start

---
+++ a01_start/a01_begin.py
@@ -0,0 +1,45 @@
+'''
+Created on 2017. 7. 18.
+
+@author: acorn
+'''
+print('hello world');
+
+
+
+if __name__ == '__main__':
+    print('hellolllllllllllllll');
+
+
+sum = 0
+for cnt in range(1,10):
+    sum+= cnt
+    print('카운팅: ', cnt)
+print(sum)
+
+x = 100
+if(x>99):
+    print('99 < ', x)
+else:
+    print('99 > ', x)
+
+
+import os
+os.chdir('C:/a01_prog/pythonexp/a01_start')
+print(os.getcwd())
+
+## https://try.jupyter.org/   > new: python3
+# import matplotlib.pyplot as plt
+# plt.plot([1,2,3,4])
+# plt.ylabel('numbers')
+# plt.show
+
+import this
+
+
+
+
+
+
+
+
---
+++ a01_start/tester.py
@@ -0,0 +1,7 @@
+'''
+Created on 2017. 7. 18.
+
+@author: acorn
+'''
+
+import this
